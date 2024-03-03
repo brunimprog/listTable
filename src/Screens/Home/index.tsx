bc8e1cfa07b17817412a8659380f934de3dc756a
Bruno <brunogteixeira18@gmail.com>
2024-03-03 15:12:57 -0300

    primeira integração

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, Button } from 'react-native';
+import { Enderecos } from '../../models/Enderecos'
 
 const Home = () => {
+
+    const [endereco, setEndereco] = useState(Array<Enderecos>);
+
+    const getEndereco = async () => {
+        const enderecoURL = 'https://viacep.com.br/ws/09852730/json/';
+
+        await fetch(enderecoURL)
+            .then((response) => response.json())
+            .then(data => {
+                setEndereco(data)
+            })
+            .catch((err) => console.log(err))
+    }
+
     return (
         <SafeAreaView>
-            <Text>Testando Home</Text>
+            <Text>Cep: {endereco.cep}</Text>
+            <Text>Logradouro: {endereco.logradouro}</Text>
+            <Text>Complemento: {endereco.complemento}</Text>
+            <Text>Bairro: {endereco.bairro}</Text>
+            <Text>endereco: {endereco.localidade}</Text>
+            <Text>UF: {endereco.uf}</Text>
+            <Text>IBGE: {endereco.ibge}</Text>
+            <Button title="Buscar Bairro" onPress={() => getEndereco()}></Button>
         </SafeAreaView>
     );
 };
